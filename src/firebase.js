f7b0df4155a1cdb4760398c18f6d33c33435fa38
codingsprout <leealansh@gmail.com>
2020-10-25 10:25:01 -0400

    starting firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCz6z8kJq9-jLbE-QCEMJ_Ki7eAvaie_Ns',
+  authDomain: 'shop-clone-babe8.firebaseapp.com',
+  databaseURL: 'https://shop-clone-babe8.firebaseio.com',
+  projectId: 'shop-clone-babe8',
+  storageBucket: 'shop-clone-babe8.appspot.com',
+  messagingSenderId: '118061060981',
+  appId: '1:118061060981:web:6f66df12cd317dc3caf93c',
+  measurementId: 'G-F701C6TH47',
+};
